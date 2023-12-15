27598013263b955cfe9f0156212ef6ed8ad41f99
Cheolmin99 <cjfals8570@naver.com>
2023-12-16 08:11:41 +0900

    props를 이용해 컴포넌트를 그대로 쓰며 값을 바꿔 나타내기

--- basic/src/AppJSX.jsx
+++ basic/src/AppJSX.jsx
@@ -1,22 +1,24 @@
 import './App.css';
 
-function App() {
+function AppJSX() {
   const name = '철민';
+  const list = ['우유', '딸기', '바나나','요거트'];
   return (
     <>
-      <h1 className='orange'>Hellow</h1>
+      <h1 className='orange'>{`hello! ${name}`}</h1>
       <h2>Hellow</h2>
       <p>{name}</p>
       <ul>
-        <li>우유</li>
-        <li>딸기</li>
+        {list.map((item) => (
+            <li>{item}</li>
+          ))}
+      </ul>
         <img 
         style={{ width: '200px', height: '200px'}}
         src="https://lh3.googleusercontent.com/XWuZGqIrIsaoKHUqqQ2rs_GhS5JaH1p5pPBIUpj22mjNRNdR3Ana8FKz4B7JwsA6HIFVXGuU7pa4ELiW6iUNhs0Iyg=s1280-w1280-h800"
         alt="nature"/>
-      </ul>
     </>
   );
 }
 
-export default App;
+export default AppJSX;
---
+++ basic/src/AppProfile.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Profile from './components/Profile';
+
+function AppProfile() {
+  return (
+    <>
+      <Profile
+        image='https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fea%2Fd5%2F86%2Fead58647a820974def08455b2d2ce70c.jpg&type=a340'
+        name='Elie'
+        title='프론트엔드 개발자'
+      />
+      <Profile
+        image='https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTExMjhfMTQz%2FMDAxNTc0OTQzMTkxMjg2.VIXTIT6lQtex8AogU23xCFHhwUaGay-lBfB6LTIyx4Eg.Vv11toBhhrrR5BGA9bOHHQ4nvjimsl6ltigdC-2WUtEg.JPEG.imagelab01%2F%25BF%25DC%25B1%25B9%25C0%25CE_%25B8%25F0%25B5%25A8_%25C1%25F6%25B8%25C1%25BB%25FD_%25C7%25C1%25B7%25CE%25C7%25CA9.jpg&type=a340'
+        name='Jhone'
+        title='백엔드 개발자'
+      />
+      <Profile
+        image='https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F609%2F2020%2F07%2F01%2F202007010819312410_7_20200701082114665.jpg&type=a340'
+        name='Anna Young'
+        title='디자이너'
+      />
+    </>
+  );
+}
+
+export default AppProfile;
