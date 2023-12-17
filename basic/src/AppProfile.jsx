4fbde15a5b31099cd24ea64411d259d6651ca1b9
Cheolmin99 <cjfals8570@naver.com>
2023-12-17 20:05:31 +0900

    state 복습 prev(이전 상태값)의 사용 방법

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -1,13 +1,24 @@
 import './App.css';
+import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    alert('클릭됨');
+  };
   return (
     <>
+    <button onClick={handleClick}>버튼</button>
+    <Avatar 
+      image="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fea%2Fd5%2F86%2Fead58647a820974def08455b2d2ce70c.jpg&type=a340"
+      isNew={true}
+    />
       <Profile
         image='https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fea%2Fd5%2F86%2Fead58647a820974def08455b2d2ce70c.jpg&type=a340'
         name='Elie'
         title='프론트엔드 개발자'
+        isNew={true}
       />
       <Profile
         image='https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxOTExMjhfMTQz%2FMDAxNTc0OTQzMTkxMjg2.VIXTIT6lQtex8AogU23xCFHhwUaGay-lBfB6LTIyx4Eg.Vv11toBhhrrR5BGA9bOHHQ4nvjimsl6ltigdC-2WUtEg.JPEG.imagelab01%2F%25BF%25DC%25B1%25B9%25C0%25CE_%25B8%25F0%25B5%25A8_%25C1%25F6%25B8%25C1%25BB%25FD_%25C7%25C1%25B7%25CE%25C7%25CA9.jpg&type=a340'
